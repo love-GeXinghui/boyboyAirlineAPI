1e2a723e9589af8763e07755978b3f6dc20c8040
赵晏伯 <77821627+love-GeXinghui@users.noreply.github.com>
2023-05-11 16:09:47 +0800

    Update urls.py

--- API/airline/urls.py
+++ API/airline/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('airline/', views.findflight.as_view()),
-    path('paymentMethod/', views.paymentMethod.as_view()),
-    path('bookingStatus/', views.bookingStatus.as_view()),
-    path('bookflight/', views.bookflight.as_view()),
-    path('payforbooking/', views.payforbooking.as_view()),
-    path('finalizebooking/', views.finalizebooking.as_view()),
-    path('cancelbooking/', views.cancelbooking.as_view()),
+    path('boyboy/findflight/', views.findflight.as_view()),
+    path('boyboy/paymentMethod/', views.paymentMethod.as_view()),
+    path('boyboy/bookingStatus/', views.bookingStatus.as_view()),
+    path('boyboy/bookflight/', views.bookflight.as_view()),
+    path('boyboy/payforbooking/', views.payforbooking.as_view()),
+    path('boyboy/finalizebooking/', views.finalizebooking.as_view()),
+    path('boyboy/cancelbooking/', views.cancelbooking.as_view()),
     ]
-
